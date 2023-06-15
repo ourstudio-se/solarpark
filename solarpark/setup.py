b1d227bb9c6da8fa6ad59e0267f45db932ed3466
Simon Hamberg <heman@ourSimon.localdomain>
2023-06-15 11:15:04 +0200

    Tagit bort api_security för hook

--- solarpark/setup.py
+++ solarpark/setup.py
@@ -24,7 +24,7 @@ def add_routes(app) -> None:
     app.include_router(
         hooks.router,
         tags=["hooks"],
-        dependencies=[Depends(get_logger), Depends(api_security)],
+        dependencies=[Depends(get_logger)],
     )
     app.include_router(
         analytics.router,
